82ec2c92f413f34c8826bd83a50fe67a3756c02e
Ivan Castelan <108840182+ivancastelan@users.noreply.github.com>
2025-06-04 22:31:25 -0600

    Update EjerciciosT1.R

--- ejercicios/EjerciciosT1.R
+++ ejercicios/EjerciciosT1.R
@@ -13,4 +13,35 @@ opBasic = function(a,b){
   print(paste(sprintf("%i : %i = ",b,a),b%/%a))
   print(paste("con resto ",b%%a))
 }
-##Comentario
+
+# Practica ----------------------------------------------------------------
+
+opBasica <- function(a,b){
+  print("Suma")
+  print(a+b)
+  print("Resta")
+  print(paste(sprintf("%i - %i = ",a,b),a-b))
+  print(paste(sprintf("%i - %i = ",b,a),b-a))
+  print("Producto")
+  print(a*b)
+  print("División")
+  print(a/b)
+  print("Cociente de la división entera")
+  print(paste(sprintf("%i entre %i = ",a,b),a%/%b))
+  print(paste("y el restante es", a%%b))
+  print("Cociente de la división entera")
+  print(paste(sprintf("%i entre %i = ",b,a),b%/%a))
+  print(paste("y el restante es", b%%a))
+}
+
+opBasica(3,2)
+
+# Numeros complejos -------------------------------------------------------
+
+com <- 2+5i
+class(com)
+
+round(complex(pi,sqrt(2)),2)
+
+round(sqrt(as.complex(-5)),3)
+ 
